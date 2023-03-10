14fd6221f6f7e7850529e01b9e40e7b9768c4f7d
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-10 19:57:38 +0300

    def help and def values

--- app.py
+++ app.py
@@ -3,13 +3,34 @@ import telebot
 
 TOKEN = '6293751402:AAEa8tUjJfcslx6tDaHcuDWXL2sGjI3t9Oc'
 
-
 bot = telebot.TeleBot(TOKEN)
 
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
+# @bot.message_handler()
+# def echo_test(message: telebot.types.Message):
+#     bot.send_message(message.chat.id, 'hello')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help_(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты> \nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
 
 
 bot.polling()
+

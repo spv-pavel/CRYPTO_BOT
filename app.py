ef533d480cceb7bcb05eec69f432243d0314f25d
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-11 13:55:05 +0300

    refactoring

--- app.py
+++ app.py
@@ -1,45 +1,9 @@
-import requests
 import telebot
-import json
+from config import keys, TOKEN
+from utils import ConversionException, CryptoConverter
 
-
-TOKEN = '6293751402:AAEa8tUjJfcslx6tDaHcuDWXL2sGjI3t9Oc'
 
 bot = telebot.TeleBot(TOKEN)
-
-keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
-    'доллар': 'USD',
-}
-
-
-class ConversionException(Exception):
-    pass
-
-
-class CryptoConverter:
-    @staticmethod
-    def convert(quote: str, base: str, amount: str):
-        if quote == base:
-            raise ConversionException(f'Невозможно перевести одинаковые валюты {base}.')
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise ConversionException(f'Не удалось обработать валюту {quote}.')
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConversionException(f'Не удалось обработать валюту {base}.')
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConversionException(f'Не удалось обработать количество {amount}.')
-
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[base_ticker] * amount
-
-        return total_base
 
 
 @bot.message_handler(commands=['start', 'help'])
